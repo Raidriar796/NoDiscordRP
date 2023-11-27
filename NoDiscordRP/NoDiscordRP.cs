4bdb1496498fe44be949ae5959ebbd1a929cd7c8
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2023-11-27 02:11:32 -0800

    First Release

---
+++ NoDiscordRP/NoDiscordRP.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+using FrooxEngine.Interfacing;
+
+namespace NoDiscordRP;
+
+public class NoDiscordRP : ResoniteMod
+{
+    public override string Name => "NoDiscordRP";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/Raidriar796/NoDiscordRP";
+    public static ModConfiguration? Config;
+
+    [AutoRegisterConfigKey] public static readonly ModConfigurationKey<bool> Enabled =
+        new ModConfigurationKey<bool>(
+            "Enabled",
+            "Allow session info to display in Discord.",
+            () => false);
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new("net.raidriar796.NoDiscordRP");
+        Config = GetConfiguration();
+        Config?.Save(true);
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(DiscordConnector), "SetCurrentStatus")]
+    class RPPatch
+    {
+        public static bool Prefix()
+        {
+            if (Config.GetValue(Enabled)) return true;
+
+            return false;
+        }
+    }
+}
